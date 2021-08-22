6a98e71343bf331ae139df1df611345d58273b4b
suryaprakash <singhyash2207@gmail.comm>
2021-08-22 11:13:22 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean playmusic = true;
+        if (playmusic) {
+            System.out.println("Music is playing");
+        } else{
+                System.out.println("Music is paused");
+            }
+
+    }
+}
